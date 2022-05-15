2f262b0431ed97e96e4077f7a2ae92c9c9b75450
era <the@communist.dev>
2022-05-15 18:23:06 +0100

    initing project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Scripting Clojure
+
+This repository contains different scripts to make my life easier. Each folder represents a category, for example the folder git has clojure scripts to handle github and codeberg.
+
+## Installing
+### Dependencies
+## Using
+
+### Git
+- new-project.cljs: setups a new project by creating a folder, initing git and creating a repo on Codeberg and Github. Also adds both urls as push url (but uses only github as pull).
---
+++ git/new-project.cljs
@@ -0,0 +1,41 @@
+(require '[babashka.curl :as curl])
+(require '[cheshire.core :as json])
+(require '[clojure.tools.cli :refer [parse-opts]])
+(require '[clojure.java.shell :as shell])
+
+(def codeberg-url "https://codeberg.org/api/v1/user/repos")
+
+(defn request-create-codeberg [name description auth_token]
+  (curl/post codeberg-url {:form-params {:auto_init "false"
+                                         :description description
+                                         :name name
+                                         :private "false"}
+                           :headers {:Authorization (apply str ["token " auth_token])}}))
+
+(defn create-codeberg [name description auth_token]
+  (request-create-codeberg name description auth_token))
+
+(def github-url "https://api.github.com/user/repos")
+
+(defn request-create-github [name description auth_token]
+  (curl/post github-url {:body (json/generate-string {:description description
+                                                      :name name})
+                           :headers {:Authorization (apply str ["token " auth_token])}}))
+(defn create-github [name description auth_token]
+  (request-create-github name description auth_token))
+
+
+(def cli-options
+  [["-n" "--name NAME" "Repo name"]
+   ["-d" "--description DESCRIPTION" "Repo description"]
+   ["-h" "--help"]])
+
+(let [{name :name, description :description} (:options (parse-opts *command-line-args* cli-options))] 
+  (shell/sh "/bin/sh" "-c" (apply str ["mkdir " (System/getenv "CODE_PATH") name ]))
+  (shell/sh "/bin/sh" "-c" (apply str ["cd " (System/getenv "CODE_PATH") name ";"
+                                       "git init; "
+                                       "git remote add origin git@github.com:" "era" "/" name " ;"
+                                       "git remote set-url --add --push origin git@codeberg.org:" "era" "/" name ";"
+                                       "git remote set-url --add --push origin git@github.com:" "era" "/" name]))
+  (create-codeberg name description (System/getenv "CODEBERG_ACCESS_TOKEN"))
+  (create-github name description (System/getenv "GITHUB_KEY")))
