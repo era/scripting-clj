567812e0ac20ef6d2e23fcaa526ccbfa58e1a5ec
era <the@communist.dev>
2022-06-15 08:03:43 +0100

    making it easier to parse output

--- git/new-project.cljs
+++ git/new-project.cljs
@@ -5,36 +5,43 @@
 
 (def codeberg-url "https://codeberg.org/api/v1/user/repos")
 
+(def github-url "https://api.github.com/user/repos")
+
 (defn request-create-codeberg [name description auth_token]
-  (curl/post codeberg-url {:form-params {:auto_init "false"
-                                         :description description
-                                         :name name
-                                         :private "false"}
+  (curl/post codeberg-url {:throw false :form-params {:auto_init "false"
+                                                      :description description
+                                                      :name name
+                                                      :private "false"}
                            :headers {:Authorization (apply str ["token " auth_token])}}))
 
 (defn create-codeberg [name description auth_token]
   (request-create-codeberg name description auth_token))
 
-(def github-url "https://api.github.com/user/repos")
-
 (defn request-create-github [name description auth_token]
-  (curl/post github-url {:body (json/generate-string {:description description
-                                                      :name name})
-                           :headers {:Authorization (apply str ["token " auth_token])}}))
+  (curl/post github-url {:throw false :body (json/generate-string {:description description
+                                                                   :name name})
+                         :headers {:Authorization (apply str ["token " auth_token])}}))
+
 (defn create-github [name description auth_token]
   (request-create-github name description auth_token))
 
+(defn check-status [http_response name_c]
+  (if (> 299 (:status http_response))
+    (println "SUCCEDED HTTP CALL " name_c)
+    (do (println "FAILED " http_response)
+        (System/exit 1))))
 
 (def cli-options
   [["-n" "--name NAME" "Repo name"]
    ["-d" "--description DESCRIPTION" "Repo description"]])
 
-(let [{name :name, description :description} (:options (parse-opts *command-line-args* cli-options))] 
-  (shell/sh "/bin/sh" "-c" (apply str ["mkdir " (System/getenv "CODE_PATH") name ]))
+(let [{name :name, description :description} (:options (parse-opts *command-line-args* cli-options))]
+  (shell/sh "/bin/sh" "-c" (apply str ["mkdir " (System/getenv "CODE_PATH") name]))
   (shell/sh "/bin/sh" "-c" (apply str ["cd " (System/getenv "CODE_PATH") name ";"
                                        "git init; "
                                        "git remote add origin git@github.com:" "era" "/" name " ;"
                                        "git remote set-url --add --push origin git@codeberg.org:" "era" "/" name ";"
                                        "git remote set-url --add --push origin git@github.com:" "era" "/" name]))
-  (create-codeberg name description (System/getenv "CODEBERG_ACCESS_TOKEN"))
-  (create-github name description (System/getenv "GITHUB_KEY")))
+
+  (check-status (create-codeberg name description (System/getenv "CODEBERG_ACCESS_TOKEN")) "codeberg")
+  (check-status (create-github name description (System/getenv "GITHUB_KEY")) "github"))
